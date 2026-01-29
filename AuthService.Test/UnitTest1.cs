2864f8119f04146a7af208452c1475de75ed8240
SinhaGautam <gsinha289@gmail.com>
2026-01-29 15:22:22 +0530

    Test Projects Added

---
+++ AuthService.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace AuthService.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ ProjectService.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace ProjectService.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
